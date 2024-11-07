d28a688d6062873a32361db96c836ed03a1040c7
Gabriel Drumond <gabriel_s4@hotmail.com>
2024-11-07 20:56:29 -0300

    feat(frontend):implementada babel e ouras dependencias.

--- core/generic.py
+++ core/generic.py
@@ -1,10 +1,16 @@
-from sqlalchemy import Column,Integer, String, Boolean,Date,ForeignKey,Null,DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column,Integer, Boolean,DateTime
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from core.configs import settings
 #from pydantic import EmailStr, use no schema e não no model
 import datetime
 
-class modelsGeneric(settings.DB_BaseModel):
-    __tablename__ = None
+@as_declarative()
+class modelsGeneric():
+
+    id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime,default=datetime.datetime.now())
     active = Column(Boolean,default=True)
+    
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
--- webapp/vue/vue.config.js
+++ webapp/vue/vue.config.js
@@ -1,24 +1,49 @@
 const webpack = require('webpack');
 const path = require("path");
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
+        '@': path.resolve(__dirname, 'src'),
         globalize$: path.resolve(__dirname, "node_modules/globalize/dist/globalize.js"),
         globalize: path.resolve(__dirname, "node_modules/globalize/dist/globalize"),
         cldr$: path.resolve(__dirname, "node_modules/cldrjs/dist/cldr.js"),
         cldr: path.resolve(__dirname, "node_modules/cldrjs/dist/cldr")
       },
-      extensions: [".ts", ".js", ".json"]
+      extensions: [".ts", ".js", ".json", ".vue"]
     },
     plugins: [
       new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false,
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
-      })
-    ]
+      }),
+      new VueLoaderPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ],
+        },
+      ],
+    },
   },
   devServer: {
     proxy: {
